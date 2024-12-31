9a27013a1c9443c17d77d0c48f4755ed0e3dafb7
Chris Hurst <46472228+ytsruh@users.noreply.github.com>
2024-12-31 13:07:50 +0000

    fix:added fzf to nevim config

--- .config/nvim/lua/config/plugins/telescope.lua
+++ .config/nvim/lua/config/plugins/telescope.lua
@@ -7,6 +7,14 @@ return {
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
     },
     config = function()
+      -- load in the native fuzzy finder extension
+      require("telescope").setup {
+	extensions = {
+	  fzf = {}
+	}
+      }
+      require("telescope").load_extensions('fzf')
+
       -- search current directory with "<space> fn"
       vim.keymap.set("n","<space>fd", require('telescope.builtin').find_files)
       -- search neovim directory with "<space> en"
