990ac3b302af022f2cdc53212dcb6b61bd626839
Chris Hurst <46472228+ytsruh@users.noreply.github.com>
2025-07-15 19:45:14 +0100

    fix: telescope searching all files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,6 +2,17 @@ return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
+        require('telescope').setup({
+            defaults = {
+                file_ignore_patterns = {
+                    "node_modules",
+                    ".wrangler",
+                    ".react-router",
+                    "build",
+                    "dist"
+                }
+            }
+        })
 	local builtin = require('telescope.builtin')
 	vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 	vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
